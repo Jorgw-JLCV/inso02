8765c19e5c3fefb2d1198d70165bc107a2fcdad3
CastilloVega <jorgecastillow4u@gmail.com>
2023-11-22 21:47:17 -0500

    3

--- src/app/pages/pago/pago.component.ts
+++ src/app/pages/pago/pago.component.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pago',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./pago.component.css']
 })
 export class PagoComponent {
+  paymentForm = this.fb.group({
+    nombre: ['', Validators.required],
+    numero_tarjeta: ['', Validators.required],
+    fecha_expiracion: ['', Validators.required],
+    codigo_verificacion: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  onSubmit() {
+      this.router.navigate(['/consulta']);
+  }
 
 }
